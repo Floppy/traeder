13725d5a17c0d1da0d924721a207d846f5e79228
Appie Verschoor <git@xiffy.nl>
2011-06-18 16:47:38 +0200

    Some basic configuration for the database

---
+++ lib/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+# some configuration
+$_GLOBALS['db_name'] = 'db96975_traeder';
+$_GLOBALS['db_user'] = 'db96975_traeder';
+$_GLOBALS['db_password'] = 'traeder1';
+$_GLOBALS['db_host'] = 'internal-db.s96975.gridserver.com';
--- web/api/index.php
+++ web/api/index.php
@@ -5,19 +5,26 @@
 
   include ('../../lib/fitzgerald.php');
 
-  class Application extends Fitzgerald {
+  class TraederApi extends Fitzgerald {
     // Define your controller methods, remembering to return a value for the browser!
-    function get_index() {
-      return page('index');
+    function get_index()
+    {
+      return 'this is the home of the traeder-api';
+    }
+
+    function get_account()
+    {
+    	return 'you want to view an account';
     }
   }
 
-  $app = new Application();
+  $app = new TraederApi();
 
 
   // Define your url mappings. Take advantage of placeholders and regexes for safety.
+  $app->get('/api/', 'get_index');
   $app->get('/account/\?.*', 'get_account');
-  $app->get('/account/create', 'create_account');
+  $app->get('/api/account/create', 'create_account');
 
   $app->get('/api/transaction/:issuecode', 'get_transaction');
 
