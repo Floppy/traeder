387a40206c759a359092563d261681883402a023
jimmytidey <jimmytidey@gmail.com>
2011-06-18 17:32:24 +0100

    added some html pages

---
+++ web/transaction.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+	<title>Traeder</title>
+	<link rel="stylesheet"  href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
+	<link rel="stylesheet" href="style/style.css" />
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+
+	<script type="text/javascript" src="/script/script.js"></script>	
+
+</head> 
+
+
+
+<body> 
+<div data-role="page" data-theme="b" id="jqm-home">
+	<div id='title'>
+		<h1>Traeder - Transaction</h1>
+
+	</div>
+	
+	<div data-role="content">
+
+
+		<lable for='login_username'>Username</label> 
+		<input type='text' id='login_username' 	name='login_username'/>
+		
+	</div>
+	
+	
+</div>
+</body>
+</html>
+
