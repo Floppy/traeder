f1d676db7a7cc86cbb1bf0d1d939368ba8f3b089
jimmytidey <jimmytidey@gmail.com>
2011-06-18 17:13:21 +0100

    db defination updated with salt

---
+++ web/new_transaction.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+	<title>Traeder</title>
+	<link rel="stylesheet"  href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
+	<link rel="stylesheet" href="style/style.css" />
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+
+</head> 
+
+<body> 
+<div data-role="page" data-theme="b" id="jqm-home">
+	<div id='title'>
+		<h1>Traeder</h1>
+		
+		<a id='log_out' href='accounts/logout'>Log Out</a>
+	</div>
+	
+	<div data-role="content">
+		
+		
+		
+	</div>
+</div>
+</body>
+</html>
+
