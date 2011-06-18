d1ca680dd47744763817a76cb43d1158968742ed
Appie Verschoor <git@xiffy.nl>
2011-06-18 19:46:23 +0200

    Moved away db variables from the config, created an example to config

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -1,10 +1,6 @@
 <?php
 # some configuration
-$_GLOBALS['db_name'] = 'db96975_traeder';
-$_GLOBALS['db_user'] = 'db96975_traeder';
-$_GLOBALS['db_password'] = 'traeder1';
-$_GLOBALS['db_host'] = 'localhost';
-
+require_once('config.local.php');
 
 $db = new mysqli($_GLOBALS['db_host'], $_GLOBALS['db_user'], $_GLOBALS['db_password'], $_GLOBALS['db_name']);
 //mysqli_select_db();
@@ -13,4 +9,3 @@ if (! is_object($db))
 	echo 'Sorry missing database';
 	exit;
 }
-
