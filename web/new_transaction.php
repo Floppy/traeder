040c6b01443f0c3f3eb3a2582e9862fbd77b6091
stronk <koen@stronk.net>
2011-06-19 14:29:20 +0100

    adding a favicon

--- web/new_transaction.php
+++ web/new_transaction.php
@@ -11,6 +11,13 @@
 	
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
 	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+	<script type="text/javascript" src="script/script.js"></script>
+
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png">
+
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 </head>  
 
 <body> 
