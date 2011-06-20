db4dc067960a6d535876d457788e64703da4c55f
Appie Verschoor <git@xiffy.nl>
2011-06-21 00:27:55 +0200

    dir-config is global

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -2,6 +2,8 @@
 # some configuration
 $_GLOBALS['app_root']	 = substr(dirname(realpath(__FILE__)), 0, strlen(dirname(realpath(__FILE__))) - strlen('/lib'));
 $_GLOBALS['kaywa_url'] = 'http://qrcode.kaywa.com/img.php?s=5';
+$_GLOBALS['qr_cache'] = $_GLOBALS['app_root'].'/web/images/qr';
+$_GLOBALS['qr_url'] = '/images/qr';
 
 require_once('config.local.php');
 require_once('global_func.php');
