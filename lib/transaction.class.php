221304a717287c151ccb8fe96f5a681e22f9a098
Appie Verschoor <git@xiffy.nl>
2011-06-21 23:45:02 +0200

    Added description to the transaction

--- lib/transaction.class.php
+++ lib/transaction.class.php
@@ -6,18 +6,19 @@
 
 class Transaction
 {
-  
+
   public $id;
   public $code;
   public $amount;
+  public $description;
   public $account_id;
-  
+
 	function __construct()
 	{
 		return true;
 	}
 
-	public static function create($acct, $amount, $pending, $code=null) {
+	public static function create($acct, $amount, $pending, $code=null, $description='') {
 	  // Make new object
 	  $tr = new Transaction;
     // Generate shortcode
@@ -34,20 +35,21 @@ class Transaction
   	$tr->account_id = $acct->id;
   	$tr->amount = $amount;
   	$tr->status = $pending ? 0 : 1;
+  	$tr->description = $description;
   	// Done
   	$tr->save();
   	return $tr;
 	}
-	
+
 	public static function find($code) {
     // Fetch from DB
-    global $db; 
-    $stmt = $db->prepare("SELECT transaction_id, amount, code, account_id, status FROM transactions WHERE code=?");
+    global $db;
+    $stmt = $db->prepare("SELECT transaction_id, amount, code, account_id, status, description FROM transactions WHERE code=?");
     $stmt->bind_param('s', $code);
     $stmt->execute();
     // Store attributes
     $tr = new Transaction();
-    $stmt->bind_result($tr->id, $tr->amount,$tr->code,$tr->account_id,$tr->status);
+    $stmt->bind_result($tr->id, $tr->amount,$tr->code,$tr->account_id,$tr->status, $tr->description);
     $stmt->fetch();
     $stmt->close();
     if($tr->id)
@@ -59,14 +61,14 @@ class Transaction
   function save() {
     global $db;
     if ($this->id) {
-      $stmt = $db->prepare("UPDATE transactions SET amount=?, code=?, account_id=?, status=? WHERE transaction_id=?");
-      $stmt->bind_param('dssii', $this->amount, $this->code, $this->account_id, $this->status, $this->id);
+      $stmt = $db->prepare("UPDATE transactions SET amount=?, code=?, account_id=?, status=?, description=? WHERE transaction_id=?");
+      $stmt->bind_param('dssisi', $this->amount, $this->code, $this->account_id, $this->status, $this->id);
       $stmt->execute();
       $stmt->close();
     }
     else {
-      $stmt = $db->prepare("INSERT INTO transactions (amount, code, account_id, status) VALUES (?,?,?,?)");
-      $stmt->bind_param('dssi', $this->amount, $this->code, $this->account_id, $this->status);
+      $stmt = $db->prepare("INSERT INTO transactions (amount, code, account_id, status,description) VALUES (?,?,?,?,?)");
+      $stmt->bind_param('dssis', $this->amount, $this->code, $this->account_id, $this->status, $this->description);
       $stmt->execute();
       $stmt->close();
       $stmt = $db->prepare("SELECT LAST_INSERT_ID()");
@@ -76,11 +78,11 @@ class Transaction
       $stmt->close();
     }
   }
-  
+
   public function account() {
     return Account::findId($this->account_id);
   }
-  
+
   public function accept($acct) {
     // Mark as no longer pending
     $this->status = 1;
@@ -96,5 +98,5 @@ class Transaction
     $stmt->execute();
     $stmt->close();
   }
-  
+
 }
